60c8f163fd2a334db161eae6d624e5ea78c04c90
Jim Madden <imadden@gmail.com>
2017-02-27 15:26:32 -0800

    initial commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+// Get the PHP helper library from twilio.com/docs/php/install
+require __DIR__ . '/vendor/autoload.php'; // Loads the library. This may vary depending on how you installed the library.
+use Twilio\Rest\Client;
+
+// Your Account Sid and Auth Token from twilio.com/user/account
+$sid = $_ENV['ACCOUNT_SID'];
+$token = $_ENV['AUTH_TOKEN'];
+$client = new Client($sid, $token);
+
+/* Download data from Twilio API */
+$messages = $client->messages->stream();
+
+// /* Browser magic */
+$filename = $sid."_sms.csv"; 
+header("Content-Type: application/csv");
+header("Content-Disposition: attachment; filename={$filename}");
+
+/* Write headers */
+$fields = array( 'SMS Message SID', 'From', 'To', 'Date Sent', 'Status', 'Direction', 'Price', 'Body' );
+echo '"'.implode('","', $fields).'"'."\n";
+
+/* Write rows */
+foreach ($messages as $sms) { 
+    $dateSent = $sms->dateSent;
+    $row = array(
+        $sms->sid,
+        $sms->from,
+        $sms->to,
+        $dateSent->format('Y-m-d H:i:s'),
+        $sms->status,
+        $sms->direction,
+        $sms->price,
+        $sms->body
+    );
+
+  echo '"'.implode('","', $row).'"'."\n"; 
+}
+
+echo $_REQUEST['sid'];
