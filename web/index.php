5d3c4848e58bd9ed966e16b47fa9d0661fdacdb4
Jim Madden <imadden@gmail.com>
2017-08-03 13:25:56 -0700

    proc change

--- web/index.php
+++ web/index.php
@@ -9,30 +9,40 @@
         <!-- Optional theme -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
+         <!-- Custom styles for this template -->
+        <link href="web/style.css" rel="stylesheet">
+
+
         <!-- Latest compiled and minified JavaScript -->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     </head>
+    <body>
+        
+        <div class="container">
+            <div class="header clearfix">
+                <h2>Use this form to generate a CSV of all your</h2>
+            </div>
+            <div class="jumbotron">
+                
+                <form action="exportlog.php" method="POST">
+                    <div class="form-group">
+                        <label for="sid">Your Twilio Account SID</label>
+                        <input type="text" class="form-control" id="sid" name="sid">
+                    </div>
+                    <div class="form-group">
+                        <label for="authToken">Your Twilio Authentaction Token</label>
+                        <input type="text" class="form-control" id="authToken" name="authToken">
+                    </div>
+                    
+                    <button type="submit" class="btn btn-default">Submit</button>
+                </form>
+            </div>
+            <div class="row marketing">
+            </div>
 
-    <form>
-        <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+            <footer class="footer">
+            </footer>
+        
         </div>
-        <div class="form-group">
-            <label for="exampleInputPassword1">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-        </div>
-        <div class="form-group">
-            <label for="exampleInputFile">File input</label>
-            <input type="file" id="exampleInputFile">
-            <p class="help-block">Example block-level help text here.</p>
-        </div>
-            <div class="checkbox">
-        <label>
-            <input type="checkbox"> Check me out
-        </label>
-        </div>
-        <button type="submit" class="btn btn-default">Submit</button>
-    </form>
-
+    </body>
 </html>
