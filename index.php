1ce82e01d2e10423c19326e4a30eb5fd949ee6f5
Jim Madden <imadden@gmail.com>
2017-08-03 13:28:23 -0700

    update index

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
          <!-- Custom styles for this template -->
-        <link href="web/style.css" rel="stylesheet">
+        <link href="style.css" rel="stylesheet">
 
 
         <!-- Latest compiled and minified JavaScript -->
